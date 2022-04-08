05d140bbb9553bf1d192bd6f0ed6fafd6662480e
yuzheng <13636836964@163.com>
2022-04-08 19:24:18 +0800

    删除一个配置类

--- src/main/java/com/lucifer/auth/config/WebMvcConfig.java
+++ src/main/java/com/lucifer/auth/config/WebMvcConfig.java
@@ -1,30 +1,30 @@
-package com.lucifer.auth.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.nio.charset.Charset;
-import java.util.List;
-
-/**
- * @author ：yuz13
- * @date ：Created in 2022/4/8 18:58
- * @description：
- * @modified By：
- * @version: $
- */
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Bean
-    public HttpMessageConverter<String> responseBodyConverter1() {
-        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
-    }
-
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(responseBodyConverter1());
-    }
-}
+//package com.lucifer.auth.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.StringHttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.nio.charset.Charset;
+//import java.util.List;
+//
+///**
+// * @author ：yuz13
+// * @date ：Created in 2022/4/8 18:58
+// * @description：
+// * @modified By：
+// * @version: $
+// */
+//
+////public class WebMvcConfig implements WebMvcConfigurer {
+////
+////    @Bean
+////    public HttpMessageConverter<String> responseBodyConverter1() {
+////        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+////    }
+////
+////    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+////        converters.add(responseBodyConverter1());
+////    }
+////}
