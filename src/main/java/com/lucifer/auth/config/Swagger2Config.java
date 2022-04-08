fa1c84c9348ee71b6df846069d00712c64a98f07
yuzheng <13636836964@163.com>
2022-04-08 19:40:35 +0800

    添加WebMvc配置类解决中文乱码问题

--- src/main/java/com/lucifer/auth/config/Swagger2Config.java
+++ src/main/java/com/lucifer/auth/config/Swagger2Config.java
@@ -32,16 +32,8 @@ import java.util.List;
  */
 @Configuration
 @EnableSwagger2
-public class Swagger2Config extends WebMvcConfigurationSupport {
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 解决静态资源无法访问
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-        // 解决swagger无法访问
-        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        // 解决swagger的js文件无法访问
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+public class Swagger2Config {
+
     @Bean
     public Docket createRestApi() {
         ParameterBuilder ticketPar = new ParameterBuilder();
--- src/main/java/com/lucifer/auth/config/WebMvcConfig.java
+++ src/main/java/com/lucifer/auth/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.Charset;
@@ -19,6 +20,9 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    /**
+     * 解决中文乱码问题
+     * */
     @Bean
     public HttpMessageConverter<String> responseBodyConverter1() {
         return new StringHttpMessageConverter(Charset.forName("UTF-8"));
@@ -26,5 +30,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(responseBodyConverter1());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 解决静态资源无法访问
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        // 解决swagger无法访问
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        // 解决swagger的js文件无法访问
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
